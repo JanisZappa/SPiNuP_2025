f2c4081749681c9974bd00d962a03bbd6b11b03d
JanisZappa <schoenau.a@googlemail.com>
2025-10-01 23:18:57 +0200

    Update Curves.cs

--- Assets/00_Scripts/Spinner/Curves.cs
+++ Assets/00_Scripts/Spinner/Curves.cs
@@ -4,6 +4,16 @@
 public class Curves : Singleton<Curves> 
 {
     public AnimationCurve swingLeanAnim;
-    
-    public static AnimationCurve SwingLeanAnim  { get { return Inst.swingLeanAnim; } }
+
+    public static AnimationCurve SwingLeanAnim
+    {
+        get
+        {
+            if (Inst == null)
+            {
+                Debug.Log("Fuuuck");
+            }
+            return Inst.swingLeanAnim;
+        }
+    }
 }
