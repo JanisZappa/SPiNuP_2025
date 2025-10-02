21e58aa37a58bb10cbb24f427aa63c2b02bf385f
JanisZappa <schoenau.a@googlemail.com>
2025-10-02 13:50:47 +0200

    Update HouseMesher.cs

--- Assets/03_Scenes/HouseGen/HouseMesher.cs
+++ Assets/03_Scenes/HouseGen/HouseMesher.cs
@@ -15,11 +15,6 @@ public class HouseMesher : MonoBehaviour
     
     private const float cellSize = HouseGen.cellSize;
     
-#if UNITY_EDITOR
-    private readonly List<Piece> failedPieces = new List<Piece>();
-    private readonly List<CornerIDs> failedCornerIds = new List<CornerIDs>();
-    private int failedPiecePick;
-#endif
     
     
     private void Awake()
@@ -34,10 +29,6 @@ public class HouseMesher : MonoBehaviour
         if (!placeMeshes)
             return;
         
-#if UNITY_EDITOR
-        failedPieces.Clear();
-        failedPiecePick = 0;
-#endif
         Level.ClearPieces();
         PlacerMeshes.Clear(cellSize);
 
@@ -48,27 +39,6 @@ public class HouseMesher : MonoBehaviour
         PlacePieces(piecer.seamBackCorners);
         PlacePieces(piecer.seams);
 
-#if UNITY_EDITOR
-        int failedCount = failedPieces.Count;
-        if (failedCount > 0)
-        {
-            Debug.LogFormat("Couldn't mesh {0} Pieces !".B_Red(), failedCount);
-            
-            failedCornerIds.Clear();
-
-            for (int i = 0; i < failedCount; i++)
-            {
-                CornerIDs cIDs = failedPieces[i].cornerIDs;
-                if (!failedCornerIds.Contains(cIDs))
-                {
-                    failedCornerIds.Add(cIDs);
-                    
-                    Debug.Log(cIDs.Log().B_Orange());
-                }
-            }  
-        }
-#endif
-		
         PlacerMeshes.Report();
     }
 
@@ -79,32 +49,8 @@ public class HouseMesher : MonoBehaviour
         for (int i = 0; i < fillCount; i++)
         {
             Piece piece = pieces[i];
-            
             bool foundPiece = true;
-            
             PlacerMeshes.Place(piece, gen, piece.side, ref foundPiece);
-            
-        #if UNITY_EDITOR
-            if(!foundPiece)
-                failedPieces.Add(piece);
-        #endif
         } 
     }
-
-    
-    #if UNITY_EDITOR
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Piece piece = failedPieces[failedPiecePick];
-            GameCam.CheckThisOut(piece.GetBounds(gen).Center, piece.side == 0? GameCam.CurrentSide.front : piece.side == -1);
-            EditorCamControll.SetEditorFocus();
-            
-            failedPiecePick = (failedPiecePick + 1).Repeat(failedPieces.Count);
-            
-            Debug.Log(piece.Log().B_Red());
-        }
-    }
-    #endif
 }
